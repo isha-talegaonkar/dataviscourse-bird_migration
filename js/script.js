070fbd8854a5bc6980462540df4e993fe6292af3
Isha Talegaonkar <ishatalegaonkar@Ishas-MacBook-Air.local>
2022-11-11 15:15:41 -0700

    script for converting text to json

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // ******* DATA LOADING *******
 // We took care of that for you
 async function loadData() {
-  const covidData = await d3.csv("data/owid-covid.csv");
-  const mapData = await d3.json("data/land-50m.json");
-  return { covidData, mapData };
+  const birdData = await d3.text("data/ebd_swahaw_202001_202012_relSep-2022/ebd_swahaw_202001_202012_relSep-2022.txt");
+  // const mapData = await d3.json("data/land-50m.json");
+  return { birdData };
 }
 
 // ******* STATE MANAGEMENT *******
@@ -22,23 +22,36 @@ const globalApplicationState = {
 loadData().then((loadedData) => {
   // console.log("Here is the imported data:", loadedData.covidData);
 
-  // Store the loaded data into the globalApplicationState
-  globalApplicationState.covidData = loadedData.covidData;
-  globalApplicationState.mapData = loadedData.mapData;
-  globalApplicationState.usa = loadedData.usa;
+  let birdData = loadedData.birdData
+  var cells = birdData.split('\n').map(function (el) { return el.split(/\t+/); });
+  var headings = cells.shift();
 
-  // Creates the view objects with the global state passed in
-  const worldMap = new MapVis(globalApplicationState);
-  const lineChart = new LineChart(globalApplicationState);
-
-  globalApplicationState.worldMap = worldMap;
-  globalApplicationState.lineChart = lineChart;
-
-  //TODO add interactions for Clear Selected Countries button
-  d3.select("#clear-button").on("click", () => {
-    d3.select("#overlay").selectAll("*").remove();
-    d3.select("#countries").selectAll("path").attr("class", "country");
-    globalApplicationState.selectedLocations = [];
-    lineChart.updateLineChart();
+  var obj = cells.map(function (el) {
+    var obj = {};
+    for (var i = 0, l = el.length; i < l; i++) {
+      obj[headings[i]] = isNaN(Number(el[i])) ? el[i] : +el[i];
+    }
+    return obj;
   });
+
+  console.log(obj)
+  // Store the loaded data into the globalApplicationState
+  // globalApplicationState.covidData = loadedData.covidData;
+  // globalApplicationState.mapData = loadedData.mapData;
+  // globalApplicationState.usa = loadedData.usa;
+
+  // // Creates the view objects with the global state passed in
+  // const worldMap = new MapVis(globalApplicationState);
+  // const lineChart = new LineChart(globalApplicationState);
+
+  // globalApplicationState.worldMap = worldMap;
+  // globalApplicationState.lineChart = lineChart;
+
+  // //TODO add interactions for Clear Selected Countries button
+  // d3.select("#clear-button").on("click", () => {
+  //   d3.select("#overlay").selectAll("*").remove();
+  //   d3.select("#countries").selectAll("path").attr("class", "country");
+  //   globalApplicationState.selectedLocations = [];
+  //   lineChart.updateLineChart();
+  // });
 });
